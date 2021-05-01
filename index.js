90f9b2a47a982990db8d4222d56cd4ab5444bb63
brugobi <brgobbi@hotmail.com>
2021-05-01 16:41:53 -0400

    add store

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// the same as: import redux from 'redux' but here is just a node application so use this bellow:
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const BUY_CAKE = 'BUY_CAKE';
 
 
@@ -18,9 +22,22 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE: return {
       ...state,
-      numOfCakes: state.numOfCakes -1
+      numOfCakes: state.numOfCakes - 1
     }
 
     default: return state
   }
 }
+
+// Holds application state
+const store = createStore(reducer);
+// Allows access to state via getState()
+console.log('Initial state', store.getState());
+// Registers listeners via subscribe(listener) 
+// and Handles unregistering of listeners via the function returned by subscribe(listener)
+const unsubscribe = store.subscribe(() => console.log('Update State', store.getState()));
+// Allows state to be updated via dispatch(action)
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
