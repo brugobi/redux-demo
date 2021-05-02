b72db53c50edc0acc5acb3fc09811ac02a0dedf4
brugobi <brgobbi@hotmail.com>
2021-05-02 08:42:34 -0400

    add actions

--- asyncActions.js
+++ asyncActions.js
@@ -3,3 +3,27 @@ const initialState = {
   users: [],
   error: ''
 }
+
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  }
+}
+
+const fetchUsersSucess = users => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+const fetchUsersFailure = error => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error
+  }
+}
