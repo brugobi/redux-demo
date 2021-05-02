2313ffe0a7df8bfe4b7c9ba68aa8425f62e107b3
brugobi <brgobbi@hotmail.com>
2021-05-01 23:37:37 -0400

    add buyIceCream

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
-const BUY_CAKE = 'BUY_CAKE';
 
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake() {
   return {
@@ -12,10 +13,17 @@ function buyCake() {
   }
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM
+  }
+}
+
 // (previousState, action) => newState
 
 const initialState = {
-  numOfCakes: 10
+  numOfCakes: 10,
+  numOfIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -23,6 +31,11 @@ const reducer = (state = initialState, action) => {
     case BUY_CAKE: return {
       ...state,
       numOfCakes: state.numOfCakes - 1
+    }
+
+    case BUY_ICECREAM: return {
+      ...state,
+      numOfIceCreams: state.numOfIceCreams - 1
     }
 
     default: return state
@@ -40,4 +53,6 @@ const unsubscribe = store.subscribe(() => console.log('Update State', store.getS
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
