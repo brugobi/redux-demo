4befd4c0ad9f9a07baed980bed0b27bfd9342074
brugobi <brgobbi@hotmail.com>
2021-05-02 08:50:29 -0400

    add reducers

--- asyncActions.js
+++ asyncActions.js
@@ -27,3 +27,27 @@ const fetchUsersFailure = error => {
     payload: error
   }
 }
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case FETCH_USERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+        error: ''
+      }
+    case FETCH_USERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        users: [],
+        error: action.payload
+      }
+  }
+}
