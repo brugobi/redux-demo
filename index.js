6ec4cc83a35243837570ac4545b9d64621b42d47
brugobi <brgobbi@hotmail.com>
2021-05-02 07:32:57 -0400

    add Middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // the same as: import redux from 'redux' but here is just a node application so use this bellow:
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
-
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
@@ -76,12 +79,12 @@ const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCreams: iceCreamReducer
 })
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 // Allows access to state via getState()
 console.log('Initial state', store.getState());
 // Registers listeners via subscribe(listener) 
 // and Handles unregistering of listeners via the function returned by subscribe(listener)
-const unsubscribe = store.subscribe(() => console.log('Update State', store.getState()));
+const unsubscribe = store.subscribe(() => {});
 // Allows state to be updated via dispatch(action)
 store.dispatch(buyCake());
 store.dispatch(buyCake());
